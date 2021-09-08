ed33702559875c68d5f8388827a6c8f172a5b2da
Blazkowicz <bonnet.nicolas66@gmail.com>
2021-09-08 11:20:44 +0200

    Ajout du moteur de rendu EJS et du dossier public

--- index.js
+++ index.js
@@ -4,27 +4,36 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 const config = require('./config.json');
 const app = express();
+
+app.set('view engine', 'ejs');
+
+
+/**
+ * Routes require
+ */
 const user = require('./app/routes/user.route');
+const study = require('./app/routes/study.route');
+const index = require('./app/routes/index.route');
 
 /**
  * Express configuration
  */
 app.use(fileUpload());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static(__dirname + '/public'));
 app.use(bodyParser.json());
 app.use(cors());
 app.use(express.static('public'));
 
-app.use('/user', user);
-
 /**
- * Check if server is started
+ * Routes settings
  */
-app.get("/", (req, res) => {
-    res.json({ message: "Comic Reader Status : OK" });
-});
+app.use('/user', user);
+app.use('/study', study);
+app.use('/', index);
+
 
 /**
  * Express Start
  */
-app.listen(config.port, function () {});
+app.listen(config.port, function () { console.log("Server started")});
