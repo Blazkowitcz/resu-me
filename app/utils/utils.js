a5edbca59b6f4488b9cbe44fc397df69b04ac945
Blazkowicz <bonnet.nicolas66@gmail.com>
2021-09-08 23:12:29 +0200

    Création du fichier utils

---
+++ app/utils/utils.js
@@ -0,0 +1,15 @@
+/**
+ * Transform a date to age
+ * @param {String} date 
+ * @returns {String}
+ */
+exports.dateToAge = function(date) {
+    let today = new Date();
+    let birthDate = new Date(date);
+    let age = today.getFullYear() - birthDate.getFullYear();
+    let m = today.getMonth() - birthDate.getMonth();
+    if (m < 0 || (m === 0 && today.getDate() < birthDate.getDate())) {
+        age--;
+    }
+    return age;
+}
